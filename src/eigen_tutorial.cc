36d01afcf794710870676d4dd6e7930e41524bef
Aidan Dunlap <aidan@cs.utexas.edu>
2021-09-07 23:59:20 -0500

    Add clang-format off/on rule for inline 2d matrix

--- src/eigen_tutorial.cc
+++ src/eigen_tutorial.cc
@@ -32,7 +32,10 @@ void DemoBasics() {
 
   cout << "Initialize a 2x2 matrix m1." << endl;
   Matrix2f m1;
-  m1 << 0, 2, 3, 0;
+  // clang-format off 
+  m1 << 0, 2, 
+        3, 0;
+  // clang-format on
   cout << "m1 = " << endl << m1 << endl;
 
   cout << "Multiply matrix times vector." << endl;
