6eafdffb68e4463f272b759fd99684b5c120705b
Elvin Yang <eyang@cs.utexas.edu>
2021-09-10 11:56:44 -0500

    Add 1D TOC Abstraction

---
+++ src/navigation/toc.cc
@@ -0,0 +1,20 @@
+#include "navigation/toc.h"
+
+#include <cmath>
+#include "eigen3/Eigen/Dense"
+#include "navigation/navigation.h"
+
+namespace navigation::toc {
+
+Plan_1D::Plan_1D(const Eigen::Vector2f kStartLoc, const Eigen::Vector2f kTargetLoc)
+    : start_loc_(kStartLoc), target_displacement_((kTargetLoc - kStartLoc).norm()) {
+  peak_speed_ = std::sqrt((2 * target_displacement_ * kMaxAccel * std::abs(kMaxDecel)) /
+                          (kMaxAccel + std::abs(kMaxDecel)));
+
+  // handle both trapezoid cases at the same time
+  peak_speed_ = std::min(kMaxSpeed, peak_speed_);
+  decel_disp_threshold_ =
+      target_displacement_ - (peak_speed_ * peak_speed_) / (2 * std::abs(kMaxDecel));
+}
+
+}  // namespace navigation::toc
---
+++ src/navigation/toc.h
@@ -0,0 +1,28 @@
+#ifndef CS393R_NAVIGATION_TOC_H_
+#define CS393R_NAVIGATION_TOC_H_
+
+#include "eigen3/Eigen/Dense"
+
+namespace navigation::toc {
+
+class Plan_1D {
+ public:
+  Plan_1D() = default;
+
+  Plan_1D(const Eigen::Vector2f kStartLoc, const Eigen::Vector2f kEndLoc);
+
+  inline float target_displacement() const { return target_displacement_; }
+  inline float peak_speed() const { return peak_speed_; }
+  inline float decel_disp_threshold() const { return decel_disp_threshold_; }
+  inline const Eigen::Vector2f& start_loc() const { return start_loc_; }
+
+ private:
+  Eigen::Vector2f start_loc_;
+  float target_displacement_;
+  float peak_speed_;
+  float decel_disp_threshold_;
+};
+
+}  // namespace navigation::toc
+
+#endif  // CS393R_NAVIGATION_TOC_H_
