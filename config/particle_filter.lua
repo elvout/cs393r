14f5f69da192761878d98d293546adfc47f89c9c
Elvin Yang <eyang@cs.utexas.edu>
2021-10-05 15:39:20 -0500

    Specify hyperparameters in configuration file

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -2,3 +2,22 @@ map = "maps/GDC1.txt"
 init_x = 14.7
 init_y = 14.24
 init_r = 0
+
+-- Error in translation caused by translation.
+motion_model_k1 = 1
+-- Error in translation caused by rotation.
+motion_model_k2 = 0.5
+-- Error in rotation caused by translation.
+motion_model_k3 = 0.5
+-- Error in rotation caused by rotation.
+motion_model_k4 = 0.5
+
+
+lidar_stddev = 0.1  -- meters, inflated
+
+-- The bound around the expected LIDAR reading in which a Gaussian
+-- distribution is used.
+sensor_model_d_short = -1.5 * lidar_stddev
+sensor_model_d_long = 1.5 * lidar_stddev
+
+sensor_model_gamma = 0.6
