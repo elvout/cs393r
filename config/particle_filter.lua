dd94539385db685678c13684d99e86c88966674a
Elvin Yang <eyang@cs.utexas.edu>
2021-10-05 20:20:36 -0500

    Update parameters
    2020-04-01-17-14-23.bag - 90º turning

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -6,18 +6,18 @@ init_r = 0
 -- Error in translation caused by translation.
 motion_model_k1 = 0.4
 -- Error in translation caused by rotation.
-motion_model_k2 = 1
+motion_model_k2 = 1.5
 -- Error in rotation caused by translation.
 motion_model_k3 = 0.4
 -- Error in rotation caused by rotation.
-motion_model_k4 = 1
+motion_model_k4 = 3
 
 
 lidar_stddev = 0.1  -- meters, inflated
 
 -- The bound around the expected LIDAR reading in which a Gaussian
 -- distribution is used.
-sensor_model_d_short = -2 * lidar_stddev
-sensor_model_d_long = 1 * lidar_stddev
+sensor_model_d_short = -2.5 * lidar_stddev
+sensor_model_d_long = 1.5 * lidar_stddev
 
-sensor_model_gamma = 0.3
+sensor_model_gamma = 0.25
