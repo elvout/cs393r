bf1fd88aacd2667e8e35b87fd108b7478e7b666e
Elvin Yang <eyang@cs.utexas.edu>
2021-10-07 18:39:34 -0500

    Increase stddev coeffs, d_long

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -4,11 +4,11 @@ init_y = 14.24
 init_r = 0
 
 -- Error in translation caused by translation.
-motion_model_k1 = 0.4
+motion_model_k1 = 0.45
 -- Error in translation caused by rotation.
-motion_model_k2 = 1.5
+motion_model_k2 = 1.6
 -- Error in rotation caused by translation.
-motion_model_k3 = 0.6
+motion_model_k3 = 0.65
 -- Error in rotation caused by rotation.
 motion_model_k4 = 3
 
@@ -17,6 +17,6 @@ lidar_stddev = 0.1  -- meters, inflated
 -- The bound around the expected LIDAR reading in which a Gaussian
 -- distribution is used.
 sensor_model_d_short = -2.5 * lidar_stddev
-sensor_model_d_long = 2 * lidar_stddev
+sensor_model_d_long = 2.5 * lidar_stddev
 
 sensor_model_gamma = 0.1
