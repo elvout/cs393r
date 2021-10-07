2a39f7dfa7fe23ddff9a722e8a1da5ecf4f04dc8
Aidan Dunlap <aidan@cs.utexas.edu>
2021-10-06 20:54:10 -0500

    Add --ws-only flag to ./simulate_nodes.sh for particle filter testing

--- simulator-nodes.sh
+++ simulator-nodes.sh
@@ -6,7 +6,7 @@
 # All command-line flags are passed to the simulator.
 #
 # Usage:
-#   $ ./simulator-nodes.sh [--localize]
+#   $ ./simulator-nodes.sh [--localize] [--ws-only]
 #
 # To inspect the session:
 #   $ tmux attach -t simulator-nodes
@@ -18,6 +18,8 @@
 # To kill the session:
 #   $ tmux kill-session -t simulator-nodes
 
+# Flags should be mutually exclusive
+
 set -e
 
 SESSION="simulator-nodes"
@@ -25,9 +27,13 @@ SESSION="simulator-nodes"
 tmux new-session -d -s "$SESSION"
 
 tmux rename-window -t 0 "roscore"
-tmux new-window -t "${SESSION}:1" -n "simulator"
-tmux new-window -t "${SESSION}:2" -n "websocket"
 
 tmux send-keys -t "roscore" "roscore" C-m
-tmux send-keys -t "simulator" "roscd ut_automata && ./bin/simulator $@" C-m
+
+if [ "$1" != "--ws-only" ]; then
+    tmux new-window -t "${SESSION}:1" -n "simulator"
+    tmux send-keys -t "simulator" "roscd ut_automata && ./bin/simulator $@" C-m
+fi
+
+tmux new-window -t "${SESSION}:2" -n "websocket"
 tmux send-keys -t "websocket" "roscd ut_automata && ./bin/websocket" C-m
