5591b1ff1d33dfd0d56041b26ad8e04bf0c4f4ee
Elvin Yang <eyang@cs.utexas.edu>
2021-10-09 15:33:47 -0500

    Import elvin's hyperparameters (squash)
    commit fc04c2b036eb600c27c63573d14ead2098420ccf
    Author: Elvin Yang <eyang@cs.utexas.edu>
    Date:   Fri Oct 8 01:54:25 2021 -0500
    
        Decrease k_4 and point cloud correlation
    
        A large k_4 value allowed the particle filter to recover when its
        angular estimate is very off during a turn. However, it often ends up
        introducing too much noise in the particles, resulting in a poor
        location estimate.
    
    commit bf1fd88aacd2667e8e35b87fd108b7478e7b666e
    Author: Elvin Yang <eyang@cs.utexas.edu>
    Date:   Thu Oct 7 18:39:34 2021 -0500
    
        Increase stddev coeffs, d_long
    
    commit a692331db7c1cf3dc46e7bb4b6845d098d1a8712
    Author: Elvin Yang <eyang@cs.utexas.edu>
    Date:   Wed Oct 6 18:32:36 2021 -0500
    
        Update parameters
    
        Increasing the correlation between particles seems to help with random
        objects that are not in the map. In general, 14-23.bag seems to behave
        much more consistently.
    
    commit dd94539385db685678c13684d99e86c88966674a
    Author: Elvin Yang <eyang@cs.utexas.edu>
    Date:   Tue Oct 5 20:20:36 2021 -0500
    
        Update parameters
    
        2020-04-01-17-14-23.bag - 90º turning
    
    commit 9ae64d06ff3c210ce1530e559ba0dabdcac0df5c
    Author: Elvin Yang <eyang@cs.utexas.edu>
    Date:   Tue Oct 5 19:39:43 2021 -0500
    
        Save parameters

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -4,20 +4,19 @@ init_y = 14.24
 init_r = 0
 
 -- Error in translation caused by translation.
-motion_model_k1 = 1
+motion_model_k1 = 0.45
 -- Error in translation caused by rotation.
-motion_model_k2 = 0.5
+motion_model_k2 = 1.6
 -- Error in rotation caused by translation.
-motion_model_k3 = 0.5
+motion_model_k3 = 0.65
 -- Error in rotation caused by rotation.
-motion_model_k4 = 0.5
-
+motion_model_k4 = 2.3
 
 lidar_stddev = 0.1  -- meters, inflated
 
 -- The bound around the expected LIDAR reading in which a Gaussian
 -- distribution is used.
-sensor_model_d_short = -1.5 * lidar_stddev
-sensor_model_d_long = 1.5 * lidar_stddev
+sensor_model_d_short = -2.5 * lidar_stddev
+sensor_model_d_long = 2.5 * lidar_stddev
 
-sensor_model_gamma = 0.6
+sensor_model_gamma = 0.12
