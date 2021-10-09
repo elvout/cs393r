a47f176e57442b8dde82d5987cf4be54afd7ce33
Elvin Yang <eyang@cs.utexas.edu>
2021-10-09 17:12:49 -0500

    Add parallel ObserveLaser impl

--- src/particle_filter/observe_laser.cc
+++ src/particle_filter/observe_laser.cc
@@ -1,8 +1,110 @@
+#include <condition_variable>
+#include <cstdio>
+#include <mutex>
+#include <thread>
 #include <vector>
 #include "eigen3/Eigen/Dense"
 #include "particle_filter.h"
 
 using std::vector;
+
+namespace {
+constexpr size_t N_THREADS = 4;
+
+class Barrier {
+ public:
+  Barrier(unsigned int count) : count(count), waiting(0), mutex(), cond() {
+    if (count == 0) {
+      printf("ERROR: BARRIER INITIALIZED WITH 0 COUNT\n");
+      exit(1);
+    }
+  }
+  Barrier(const Barrier&) = delete;
+  Barrier& operator=(const Barrier&) = delete;
+
+  void wait() {
+    std::unique_lock<std::mutex> lock(mutex);
+
+    waiting++;
+    if (waiting == count) {
+      waiting = 0;
+      cond.notify_all();
+      lock.unlock();
+    } else {
+      cond.wait(lock);
+      lock.unlock();
+    }
+  }
+
+ private:
+  const unsigned int count;
+  unsigned int waiting;
+  std::mutex mutex;
+  std::condition_variable cond;
+};
+
+struct WorkerArgs {
+  const size_t id;
+
+  particle_filter::ParticleFilter* pfilter;
+  std::vector<particle_filter::Particle>* particles;
+  size_t start_idx;
+  size_t end_idx;
+
+  std::vector<float>* sampled_ranges;
+  float range_min;
+  float range_max;
+  float angle_min;
+  float angle_max;
+
+  WorkerArgs(size_t id) : id(id) {}
+};
+
+std::vector<WorkerArgs> worker_args;
+std::vector<std::thread> threads;
+Barrier work_ready(N_THREADS + 1);
+Barrier comp_done(N_THREADS + 1);
+
+void worker_function(WorkerArgs* args) {
+  while (true) {
+    work_ready.wait();
+
+    particle_filter::ParticleFilter* pfilter = args->pfilter;
+    std::vector<particle_filter::Particle>& particles = *args->particles;
+    size_t start_idx = args->start_idx;
+    size_t end_idx = args->end_idx;
+    std::vector<float>& ranges = *args->sampled_ranges;
+    float range_min = args->range_min;
+    float range_max = args->range_max;
+    float angle_min = args->angle_min;
+    float angle_max = args->angle_max;
+
+    for (size_t idx = start_idx; idx < end_idx; idx++) {
+      pfilter->Update(ranges, range_min, range_max, angle_min, angle_max, particles[idx]);
+    }
+
+    comp_done.wait();
+  }
+}
+
+struct InitObj {
+  InitObj() {
+    threads.reserve(N_THREADS);
+    worker_args.reserve(N_THREADS);
+
+    for (size_t i = 0; i < N_THREADS; i++) {
+      worker_args.emplace_back(i);
+    }
+
+    for (size_t i = 0; i < N_THREADS; i++) {
+      WorkerArgs* arg = &worker_args[i];
+      threads.emplace_back([arg] { worker_function(arg); });
+    }
+  }
+};
+
+static InitObj _init;
+}  // namespace
 
 namespace particle_filter {
 
@@ -40,10 +142,22 @@ void ParticleFilter::ObserveLaser(const vector<float>& ranges,
   const float sample_angle_max =
       angle_max - (angle_max - angle_min) / ranges.size() * (ranges.size() % 10);
 
-  // TODO: parallelize this loop
-  for (Particle& p : particles_) {
-    Update(ranges_sample, range_min, range_max, angle_min, sample_angle_max, p);
+  size_t num_particles = particles_.size();
+  for (size_t i = 0; i < N_THREADS; i++) {
+    WorkerArgs& args = worker_args[i];
+
+    args.pfilter = this;
+    args.particles = &particles_;
+    args.start_idx = num_particles * i / N_THREADS;
+    args.end_idx = num_particles * (i + 1) / N_THREADS;
+    args.sampled_ranges = &ranges_sample;
+    args.range_min = range_min;
+    args.range_max = range_max;
+    args.angle_min = angle_min;
+    args.angle_max = sample_angle_max;
   }
+  work_ready.wait();
+  comp_done.wait();
 
   // Resample every n updates
   static int num_of_updates_since_last_resample = 0;
