1ec0e77a6155fe660815683dd8ae89d57a8107e2
Elvin Yang <eyang@cs.utexas.edu>
2021-11-20 02:17:48 -0600

    Reduce spread of motion model

--- config/particle_filter.lua
+++ config/particle_filter.lua
@@ -4,20 +4,20 @@ init_y = 14.24
 init_r = 0
 
 -- Error in translation caused by translation.
-motion_model_k1 = 0.45
+-- motion_model_k1 = 0.45
 -- Error in translation caused by rotation.
-motion_model_k2 = 1.6
+-- motion_model_k2 = 1.6
 -- Error in rotation caused by translation.
-motion_model_k3 = 0.65
+-- motion_model_k3 = 0.65
 -- Error in rotation caused by rotation.
-motion_model_k4 = 2.3
+-- motion_model_k4 = 2.3
 
-lidar_stddev = 0.1  -- meters, inflated
+-- lidar_stddev = 0.1  -- meters, inflated
 
 -- The bound around the expected LIDAR reading in which a Gaussian
 -- distribution is used.
-sensor_model_d_short = -2.5 * lidar_stddev
-sensor_model_d_long = 2.5 * lidar_stddev
+-- sensor_model_d_short = -2.5 * lidar_stddev
+-- sensor_model_d_long = 2.5 * lidar_stddev
 symmetric_threshold = 2.5
 
 sensor_model_gamma = 0.12
